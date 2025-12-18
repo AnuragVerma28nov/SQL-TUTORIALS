659463e39d3091d6fc8a7bf6e6bafd06bd3ad8bd
AnuragVerma28nov <anuragverma28nov@gmail.com>
2025-12-18 23:24:48 +0530

    DQL or DRL Commands

--- dql_or_drl_commands.sql
+++ dql_or_drl_commands.sql
@@ -25,16 +25,39 @@ SELECT column_name
 FROM table_name
 WHERE column_name LIKE pattern; (%@gmail.com, %mobile%, _A, A_)
 
--- 3. SELECT with ORDER BY
-SELECT first_name, hire_date
-FROM employees
-ORDER BY hire_date DESC;
+-- 5. SELECT with IN
+SELECT column1, column2
+FROM table_name
+WHERE column_name IN (value1, value2, value3);
 
--- 4. SELECT with GROUP BY and HAVING
+-- 6. SELECT with BETWEEN
+SELECT column1, column2
+FROM table_name
+WHERE column_name BETWEEN value1 AND value2;
+                
+-- 7. SELECT with NOT BETWEEN
+SELECT column1, column2
+FROM table_name
+WHERE column_name NOT BETWEEN value1 AND value2;
+
+-- 8. SELECT with NOT NULL
+SELECT column1, column2
+FROM table_name
+WHERE column_name IS NOT NULL;
+
+-- 9. SELECT with AS
+SELECT column_name AS alternate_name
+FROM table_name;
+
+-- 10. SELECT with ORDER BY
+SELECT column1, column2
+FROM table_name
+ORDER BY column_name ASC/DESC;
+
+-- 11. SELECT with GROUP BY
 SELECT department, COUNT(*) AS total_employees
 FROM employees
-GROUP BY department
-HAVING COUNT(*) > 5;
+GROUP BY department;
 
 
 
