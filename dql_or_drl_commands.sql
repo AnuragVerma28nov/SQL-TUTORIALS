92efdabe57e32e3088e4b2a534a78dd441225aab
AnuragVerma28nov <anuragverma28nov@gmail.com>
2025-12-18 18:56:38 +0530

    dql or drl commands

---
+++ dql_or_drl_commands.sql
@@ -0,0 +1,34 @@
+-- Syntax
+SELECT column1, column2, ...
+FROM table_name
+[WHERE condition]
+[GROUP BY column]
+[HAVING condition]
+[ORDER BY column ASC|DESC]
+[LIMIT number];
+
+-- 1. Basic SELECT
+SELECT first_name, last_name
+FROM employees;
+
+-- 2. SELECT with WHERE
+SELECT *
+FROM employees
+WHERE department = 'Sales';
+
+-- 3. SELECT with ORDER BY
+SELECT first_name, hire_date
+FROM employees
+ORDER BY hire_date DESC;
+
+-- 4. SELECT with GROUP BY and HAVING
+SELECT department, COUNT(*) AS total_employees
+FROM employees
+GROUP BY department
+HAVING COUNT(*) > 5;
+
+-- 5. SELECT with DISTINCT
+SELECT DISTINCT department
+FROM employees;
+
+
