af1f20a9908d1a0ba79044020dc4fa8c0685cb72
AnuragVerma28nov <anuragverma28nov@gmail.com>
2025-12-18 18:55:12 +0530

    ddl commands

---
+++ ddl_commands.sql
@@ -0,0 +1,26 @@
+-- 1. CREATE
+-- Syntax
+CREATE TABLE table_name
+(
+column_1 datatype,
+column_2 datatype,
+column_3 datatype,
+....
+);
+
+-- 2. ALTER
+-- Syntax
+ALTER TABLE table_name
+ADD column_name datatype;
+
+-- 3.TRUNCATE
+-- Syntax
+TRUNCATE TABLE table_name;
+
+-- 4.DROP
+-- Syntax
+DROP TABLE table_name;
+
+-- 5.RENAME
+-- Syntax
+RENAME TABLE Table_Name To New_Table_Name;
