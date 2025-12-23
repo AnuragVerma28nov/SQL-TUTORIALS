9d02cd2ecf0affeaf2ffe79d3db1353070e6675a
AnuragVerma28nov <anuragverma28nov@gmail.com>
2025-12-23 14:27:48 +0530

    data defination language commands

--- ddl_commands.sql
+++ ddl_commands.sql
@@ -1,10 +1,13 @@
+-- DDL COMMANDS
+
 -- 1. CREATE
 -- Syntax
+CREATE DATABASE database_name;
 CREATE TABLE table_name
 (
-column_1 datatype,
-column_2 datatype,
-column_3 datatype,
+column_1 datatype CONSTRAINT,
+column_2 datatype CONSTRAINT,
+column_3 datatype CONSTRAINT,
 ....
 );
 
@@ -20,7 +23,13 @@ TRUNCATE TABLE table_name;
 -- 4.DROP
 -- Syntax
 DROP TABLE table_name;
+DROP DATABASE database_name;
 
 -- 5.RENAME
 -- Syntax
 RENAME TABLE Table_Name To New_Table_Name;
+RENAME DATABASE Database_Name To New_Database_Name;
+
+-- 6.COMMENT
+COMMENT ON TABLE table_name IS 'Comment';
+COMMENT ON COLUMN table_name.column_name IS 'Comment';
