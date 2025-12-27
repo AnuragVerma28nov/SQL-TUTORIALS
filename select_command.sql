8623cf6c865af66bd12414ddd6b52a6d77fb7571
AnuragVerma28nov <anuragverma28nov@gmail.com>
2025-12-27 09:07:50 +0530

    selectcommand

---
+++ select_command.sql
@@ -0,0 +1,16 @@
+--Selcet Command
+
+--1.Syntax
+SELECT * FROM TableName;
+--Example
+SELECT * FROM StudentInfo.student;
+
+--2.Syntax
+SELECT Col1, Col2 FROM TableName;
+--Example
+SELECT id, name FROM sqltutorials.student;
+
+--3.Syntax
+SELECT * FROM Database.TableName;
+--Example
+SELECT * FROM sqltutorials.student;
