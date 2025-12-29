ef3bb193668c7a8b1ce43ec76dbe7435eec4cc3e
AnuragVerma28nov <anuragverma28nov@gmail.com>
2025-12-29 18:39:06 +0530

    create table command

---
+++ 1.DDL COMMANDS/create_table_command.sql
@@ -0,0 +1,13 @@
+-- Create Table Commands
+
+-- Syntax
+CREATE TABLE TableName(
+    column_name1 DATATYPE CONSTRAINT,
+    column_name1 DATATYPE CONSTRAINT,
+    );
+
+-- Example:
+CREATE TABLE Student(
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    );
